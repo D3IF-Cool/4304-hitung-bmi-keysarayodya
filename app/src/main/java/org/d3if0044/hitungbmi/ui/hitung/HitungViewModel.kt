3430c31fed0882b2b007aedc821ff741fea21ff9
keysarayodya <keysarayodya@gmail.com>
2021-04-29 18:41:45 +0700

    Menyimpan data ke database

--- app/src/main/java/org/d3if0044/hitungbmi/ui/hitung/HitungViewModel.kt
+++ app/src/main/java/org/d3if0044/hitungbmi/ui/hitung/HitungViewModel.kt
@@ -3,16 +3,26 @@ package org.d3if0044.hitungbmi.ui.hitung
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.d3if0044.hitungbmi.data.HasilBmi
 import org.d3if0044.hitungbmi.data.KategoriBmi
+import org.d3if0044.hitungbmi.db.BmiDao
+import org.d3if0044.hitungbmi.db.BmiEntity
 
-class HitungViewModel : ViewModel() {
+class HitungViewModel(private val db: BmiDao) : ViewModel() {
 
     // Hasil BMI bisa null jika pengguna belum menghitung BMI
     private val hasilBmi = MutableLiveData<HasilBmi?>()
 
     // Navigasi akan bernilai null ketika tidak bernavigasi
     private val navigasi = MutableLiveData<KategoriBmi?>()
+
+    // Variabel ini sudah berupa LiveData (tidak mutable),
+    // sehingga tidak perlu dijadikan private
+    val data = db.getLastBmi()
 
     fun hitungBmi(berat: String, tinggi: String, isMale: Boolean) {
         val tinggiCm = tinggi.toFloat() / 100
@@ -32,6 +42,17 @@ class HitungViewModel : ViewModel() {
             }
         }
         hasilBmi.value = HasilBmi(bmi, kategori)
+
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                val dataBmi = BmiEntity(
+                    berat = berat.toFloat(),
+                    tinggi = tinggi.toFloat(),
+                    isMale = isMale
+                )
+                db.insert(dataBmi)
+            }
+        }
     }
 
     fun mulaiNavigasi() {
