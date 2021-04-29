6abc197fc8449301693de8f2d21b15d7917d3dcf
keysarayodya <keysarayodya@gmail.com>
2021-04-29 19:09:47 +0700

    Menampilkan data dalam bentuk list

---
+++ app/src/main/java/org/d3if0044/hitungbmi/data/HitungBmi.kt
@@ -0,0 +1,23 @@
+package org.d3if0044.hitungbmi.data
+
+import org.d3if0044.hitungbmi.db.BmiEntity
+
+object HitungBmi {
+    fun hitung(data: BmiEntity): HasilBmi {
+        val tinggiCm = data.tinggi / 100
+        val bmi = data.berat / (tinggiCm * tinggiCm)
+        val kategori = if (data.isMale) {
+            when {
+                bmi < 20.5 -> KategoriBmi.KURUS
+                bmi >= 27.0 -> KategoriBmi.GEMUK
+                else -> KategoriBmi.IDEAL
+            }
+        } else {
+            when {
+                bmi < 18.5 -> KategoriBmi.KURUS
+                bmi >= 25.0 -> KategoriBmi.GEMUK
+                else -> KategoriBmi.IDEAL
+            }
+        }
+        return HasilBmi(bmi, kategori)
+    } }
